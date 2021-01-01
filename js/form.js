c058e4a9008b11e489c36246cbab9055e1583a39
sagarsaurabhssnl <56288082+sagarsaurabhssnl@users.noreply.github.com>
2021-01-01 22:12:56 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -32,17 +32,21 @@ class Form {
         this.reset.style('background', 'lightpink');
 
         this.button.mousePressed(() => {
-            this.input.hide();
-            this.button.hide();
-            player.name = this.input.value();
-            playerCount += 1;
-            player.index = playerCount;
-            player.update();
-            player.updateCount(playerCount);
-            this.greeting.html("Hello " + player.name)
-            this.greeting.position(400, 250);
-            this.greeting.style('color', 'white');
-            this.greeting.style('font-size', '100px');
+            if (this.input.value() !== "") {
+                this.input.hide();
+                this.button.hide();
+                player.name = this.input.value();
+                playerCount += 1;
+                player.index = playerCount;
+                player.update();
+                player.updateCount(playerCount);
+                this.greeting.html("Hello " + player.name)
+                this.greeting.position(400, 250);
+                this.greeting.style('color', 'white');
+                this.greeting.style('font-size', '100px');
+            } else if (this.input.value() === "") {
+                alert("Name cannot be empty");
+            }
         });
 
         this.reset.mousePressed(() => {
